d9bb65f9108619e57e545b7a95cc9f1523d2c72c
CodingGirl <spunase@gmail.com>
2020-09-17 16:37:17 -0500

    filter datetime

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -20,3 +20,31 @@ var button = d3.select("#filter-btn");
 button.on("click", function() {
     console.log(d3.event.target);
 });
+
+// Select the button
+var button = d3.select("#filter-btn");
+
+// Select the form
+var form = d3.select("#filters");
+
+// Create event handlers 
+button.on("click", runEnter);
+form.on("submit",runEnter);
+// Complete the event handler function for the form
+function runEnter() {
+
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
+    // Select the input element and get the raw HTML node
+    var inputElement = d3.select("#datetime");
+    // Get the value property of the input element
+    var inputValue = inputElement.property("value");
+    console.log(inputValue);
+    console.log(tableData);
+
+    // Use the form input to filter the data by datetime
+  var ufodata = tableData.filter(ufo => ufo.datetime === inputValue); 
+  console.log(ufodata);
+};
+// remove any children from the list to
+list.html("");
